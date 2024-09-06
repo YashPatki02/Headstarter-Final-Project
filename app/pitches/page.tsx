e5bd160d35272f4d573f849e47203e1869eb1f22
Yash Patki <yashom.patki@gmail.com>
2024-09-06 13:23:12 -0700

    Added ProfileEdit

--- app/pitches/page.tsx
+++ app/pitches/page.tsx
@@ -1,11 +1,13 @@
 import Profile from "@/components/Profile";
+import ProfileEdit from "@/components/ProfileEdit";
 import React from "react";
 
 const PitchesPage = () => {
     return (
         // <div>PitchesPage</div>
         <>
-            <Profile />
+            {/* <Profile /> */}
+            <ProfileEdit />
         </>
     );
 };
